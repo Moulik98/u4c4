f07c72e0516f4b08b53c57b05940c257aa73ac73
Soumya <soumamoulik@gmail.com>
2022-03-28 11:00:31 +0530

    update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,4 +6,13 @@ const client=require("../configs/redis");
 const user = require("../models/user.model");
 const router = express.Router();
 
-router.post("",async(req,res))
+router.post("",async(req,res)=>{
+    try{
+        const user =await user.create(req.body);
+        const users = await user.find().lean().exec();
+        client.set("users",JSON.stringify(users));
+        return res.status(201).send(user)
+    }catch{
+        return res.status(500).send({message: err.message})
+    }
+})
